cf2c8cab50d32fd6d632bb631ca64b706f212b1e
KendrickPC <kennethpchang@gmail.com>
2018-01-02 18:16:29 +0800

    working POST function for two pages

--- webserver.py
+++ webserver.py
@@ -1,40 +1,68 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
-
+import cgi
 
 # Specifies what code to execute
-class WebServerHandler(BaseHTTPRequestHandler):
+class webServerHandler(BaseHTTPRequestHandler):
 
     # Handles all get requests my webserver receives
     def do_GET(self):
-        if self.path.endswith("/hello"):
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            message = ""
-            message += "<html><body>Hello!</body></html>"
-            self.wfile.write(message)
-            print message
-            return
+        try:
+            if self.path.endswith("/hello"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                output = ""
+                output += "<html><body>"
+                output += "<h1>Hello!</h1>"
+                output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What did you just say to me? Say it again! I triple dipple dog dare you!</h2><input name="message" type="text"><input type="submit" value="Submit"> </form>'''
+                output += "</body></html>"
+                self.wfile.write(output)
+                print output
+                return
 
-        if self.path.endswith("/hola"):
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            message = ""
-            message += "<html><body>&#161 Hola !</body></html>"
-            self.wfile.write(message)
-            print message
-            return
+            if self.path.endswith("/hola"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                output = ""
+                output += "<html><body>"
+                output += "<h1>&#161 Hola !</h1>"
+                output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What did you just say to me? Say it again! I triple dipple dog dare you!</h2><input name="message" type="text"><input type="submit" value="Submit"> </form>'''
+                output += "</body></html>"
+                self.wfile.write(output)
+                print output
+                return
 
-        else:
+        except IOError:
             self.send_error(404, 'File Not Found: %s' % self.path)
 
+    #Adding POST to my webserver
+    def do_POST(self):
+        try:
+            self.send_response(301)
+            self.send_header('Content-type', 'text,html')
+            self.end_headers()
+            ctype, pdict = cgi.parse_header(
+                self.headers.getheader('content-type'))
+            if ctype == 'multipart/form-data':
+                fields = cgi.parse_multipart(self.rfile, pdict)
+                messagecontent = fields.get('message')
+            output = ""
+            output += "<html><body>"
+            output += "<h2> Please Work: </h2>"
+            output += "<h1> %s </h1>" % messagecontent[0]
+            output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What did you just say to me? Say it again! I triple dipple dog dare you!</h2><input name="message" type="text"><input type="submit" value="Submit"> </form>'''
+            output += "</body></html>"
+            self.wfile.write(output)
+            print output
+        except: 
+            pass
 
 # Instantiate our server and specify port 5000
 def main():
     try:
         port = 5000
-        server = HTTPServer(('', port), WebServerHandler)
+        server = HTTPServer(('', port), webServerHandler)
         print "Web Server running on port %s" % port
         server.serve_forever()
     except KeyboardInterrupt:
