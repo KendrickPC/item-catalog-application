991f2b21338723d0b06f799bc98912f8ef090ff7
KendrickPC <kennethpchang@gmail.com>
2018-01-24 15:44:48 +0800

    changed made to main.html

--- database_setup.py
+++ database_setup.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy import create_engine
